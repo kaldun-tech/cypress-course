611f077c0aae153305398ef9759d2e7d2d028975
Taras <tsmereka@protonmail.com>
2023-08-13 17:13:53 -0500

    Finished up forms

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -4,5 +4,22 @@ describe('form tests', () => {
     })
     it('Test subscribe form', () => {
         cy.contains(/testing forms/i)
+        cy.getDataTest('subscribe-form').find('input').as('subscribe-input')
+        cy.get('@subscribe-input').type('taras@kaldun.com')
+        cy.contains(/Successfully subbed: taras@kaldun.com!/i).should('not.exist')
+        cy.getDataTest('subscribe-button').click()
+        cy.contains(/Successfully subbed: taras@kaldun.com!/i).should('exist')
+        cy.wait(3000)
+        cy.contains(/Successfully subbed: taras@kaldun.com!/i).should('not.exist')
+
+        cy.get('@subscribe-input').type('taras@kaldun.tech')
+        cy.contains(/Invalid email: taras@kaldun.tech!/i).should('not.exist')
+        cy.getDataTest('subscribe-button').click()
+        cy.contains(/Invalid email: taras@kaldun.tech!/i).should('exist')
+        cy.wait(3000)
+
+        cy.contains(/fail!/i).should('not.exist')
+        cy.getDataTest('subscribe-button').click()
+        cy.contains(/fail!/i).should('exist')
     })
 })
